aa6df0c08a6b3c064d9960b5383acf14fa86ce88
Lenard Carroll <86952266+lenardcarroll@users.noreply.github.com>
2022-04-28 10:34:59 +0200

    Create ShortcutHeader.h

---
+++ ShortcutHeader.h
@@ -0,0 +1,8 @@
+#include <string>
+#include <list>
+#ifndef SHORTCUTHEADER_H
+#define SHORTCUTHEADER_H
+uint64_t getIndexInt(std::list<uint64_t> l, uint64_t var);
+uint64_t getIndexStr(std::list < std::string> l, std::string var);
+uint64_t getIndexDoub(std::list<double> l, double var);
+#endif
